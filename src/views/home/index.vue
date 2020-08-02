6514d2dc4f5381e8ab3ebbe525a3e59a8bb09b1e
王得周 <823316947@qq.com>
2020-08-02 23:15:24 +0800

    频道和文章-获取真实频道展示-制作api函数

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,64 +7,48 @@
     -->
     <van-tabs v-model="activeChannelIndex">
       <!-- <van-tab title="标签名称">当前标签对应的内容</van-tab> -->
-      <van-tab title="推荐">
-        <div class="scroll-wrapper">
-          <!-- 瀑布流加载效果(动作-上拉)
-        v-model="loading" 加载动画效果(加载中...)
-        :finished="finished" 是否停止加载，false可以继续加载,true停止加载
-        finished-text="没有更多了" 停止加载后的文字提示
-        @load="onLoad" 加载数据的回调事件，页面初次载入会自动触发
-        或者是 滚轮 滚动到系统设置的页面底部，也会自动触发
-          -->
-          <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-            <!--
-          单元格组件
-          内容独占一行进行显示，使用其他div也可以
-          title：设置单元格标题
-            -->
-            <van-cell v-for="item in list" :key="item" :title="item" />
-          </van-list>
-        </div>
+      <van-tab :title="item.name" v-for="item in channelList" :key="item.id">
+        <com-article></com-article>
       </van-tab>
-      <van-tab title="数据库">数据库内容展示</van-tab>
-      <van-tab title="后端">后端内容展示</van-tab>
+      <!-- <van-tab title="数据库">
+        <com-article></com-article>
+      </van-tab>
+      <van-tab title="后端">
+        <com-article></com-article>
+      </van-tab>-->
     </van-tabs>
   </div>
 </template>
 
 <script>
+
+// 导入api函数
+import { apiChannelList } from '@/api/channel.js'
+
+import ComArticle from './components/com-article'
+
 export default {
   name: 'home-index',
+  components: {
+    ComArticle
+  },
   data () {
     return {
-      // 瀑布流相关成员
-      list: [], // 接收加载好的数据
-      loading: false, // “加载中...”动画是否显示(加载的时候才设置为true，加载完毕再设置为false)，每次onLoad执行就设置为true，执行完毕就设置为false
-      finished: false, // 加载是否停止的标志，false可以继续加载，true瀑布流停止加载，如果后端没有数据可以提供了，就设置该项目为true即可
+      channelList: [],
       // 激活频道下标标志
       activeChannelIndex: 0
     }
   },
+  created () {
+    this.getChannelList()
+  },
   methods: {
-    // 瀑布流加载执行的方法
-    onLoad () {
-      // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      // setTimeout设置延迟加载效果，有1s延迟
-      setTimeout(() => {
-        // 把数字信息追加给list数字
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-
-        // 加载状态结束
-        this.loading = false // '加载中。。'动画清除
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true // 停止瀑布流加载
-        }
-      }, 1000)
+    // 频道
+    async getChannelList () {
+      const result = await apiChannelList()
+      console.log(result)
+      // data接收频道列表
+      this.channelList = result.channels
     }
   }
 }
@@ -85,10 +69,6 @@ export default {
   /deep/ .van-tab__pane {
     height: 100%;
     // 给上拉列表设置样式
-    .scroll-wrapper {
-      height: 100%;
-      overflow-y: auto;
-    }
   }
   // 给频道下边沿横向设置样式
   /deep/ .van-tabs__line {
