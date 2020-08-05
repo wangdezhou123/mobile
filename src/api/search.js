5cfc947950049d07688e9a7bd2aea2cd10f768d7
王得周 <823316947@qq.com>
2020-08-06 02:06:10 +0800

    搜索结果制作完成-渲染数据

---
+++ src/api/search.js
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+
+/**
+ * 获得联想建议数据
+ * @param {联想的关键字} q
+ */
+export function apiSuggestionList ({ q }) {
+  return request({
+    url: '/app/v1_0/suggestion',
+    method: 'get',
+    params: {
+      q
+    }
+  })
+}
+
+/**
+ * 根据联想关键字 搜索文章
+ * @param {联想的关键字} q
+ * @param {页码} page
+ * @param {返回内容条数} per_page
+ */
+export function apiSearchList ({ q, page = 1, per_page = 10 }) {
+  return request({
+    url: '/app/v1_0/search',
+    method: 'get',
+    params: {
+      q,
+      page,
+      per_page
+    }
+  })
+}
--- src/views/search/result.vue
+++ src/views/search/result.vue
@@ -1,13 +1,71 @@
 <template>
   <div class="container">
-    <van-nav-bar title="搜索结果" left-arrow @click-left="$router.back()" />
+    <!--给导航栏设置fixed，使得固定定位-->
+    <van-nav-bar fixed title="搜索结果" left-arrow @click-left="$router.back()" />
+    <!-- 瀑布流加载 -->
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+      <van-cell v-for="item in searchList" :key="item.art_id.toString()" :title="item.title" />
+    </van-list>
   </div>
 </template>
 
 <script>
+// 引入api接口
+import { apiSearchList } from '@/api/search'
+
 export default {
-  name: 'search-result'
+  name: 'search-result',
+  computed: {
+    // 获得路由参数q，使得组件内部可以通过 this.q 的简便方式获得路由参数
+    q () {
+      return this.$route.params.q
+    }
+  },
+  data () {
+    return {
+      page: 1, // 页码
+      per_page: 10, // 条数
+      searchList: [], // 搜索结果
+      // 瀑布相关
+      list: [],
+      loading: false,
+      finished: false
+
+    }
+  },
+  methods: {
+    // 瀑布加载
+    async onLoad () {
+      await this.$sleep(1000) // 暂停1s，不要太着急加载
+
+      const result = await apiSearchList({ q: this.q, page: this.page, per_page: this.per_page })
+      // 加载状态结束
+      this.loading = false
+
+      if (!result.results.length) {
+        // 数据全部加载完成
+        this.finished = true
+        return false
+      }
+      // 追加数据
+      this.searchList.push(...result.results)
+      // 页码累加
+      this.page++
+    }
+  }
 }
 </script>
 
-<style scoped lang='less'></style>
+<style lang="less" scoped>
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  box-sizing: border-box;
+  .van-list {
+    flex: 1;
+    margin-top: 92px;
+  }
+}
+</style>
